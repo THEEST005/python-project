7d64dd9627142fd3afb11f615275b0243d9d03ba
eMobilis <elikibetael4@gmail.com>
2023-02-13 13:16:20 +0300

    functions multiply

--- Functions/functions_ex_3.py
+++ Functions/functions_ex_3.py
@@ -1,6 +1,9 @@
-def age():
-  age=int(input("Enter Your Age"))
-  print(age)
-  result=age*12
-  print(f"Age in months is {result}")
-age()
+try:
+  def age():
+    age=int(input("Enter Your Age"))
+    print(age)
+    result=age*12
+    print(f"Age in months is {result}")
+  age()
+except:
+     print("Not a Number")
